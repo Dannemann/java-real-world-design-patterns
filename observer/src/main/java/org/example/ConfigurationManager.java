666ab3a890479407f22cfd9fb8d71f892452331e
Jean Dannemann Carone <jean.carone@gmail.com>
2023-01-13 12:01:53 -0300

    Tweaks.

--- observer/src/main/java/org/example/ConfigurationManager.java
+++ observer/src/main/java/org/example/ConfigurationManager.java
@@ -11,7 +11,15 @@ import org.example.configurators.Configurator;
  */
 public class ConfigurationManager {
 
+	/**
+	 * The observer pattern can cause memory leaks, known as the lapsed listener
+	 * problem, because in a basic implementation, it requires both explicit
+	 * registration and explicit deregistration, as in the dispose pattern, because
+	 * the subject holds strong references to the observers, keeping them alive.
+	 * This can be prevented if the subject holds weak references to the observers.
+	 */
 	private List<Configurator> configurators = new ArrayList<Configurator>();
+
 	private Map globalSettings;
 
 	public void attach(Configurator configurator) {
